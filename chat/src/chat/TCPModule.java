de4c94dc174460da26a8ae6053c630b9e38395bf
Misha Chandan <misha@purefocus.com>
2015-11-08 13:37:45 -0800

    TCP basic chat

--- chat/src/chat/TCPModule.java
+++ chat/src/chat/TCPModule.java
@@ -9,11 +9,28 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class TCPModule {
-	public static void main(String[] args) throws IOException {
-		TCPServer.createServer(4542);
-		TCPClient.createClient("localhost", 4542);
+
+	public static int PORTNO = 4541;
+	public static String SERVER_IP = "localhost";
+	public static int SERVER_PORT = 4542;
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		if (args.length != 3) {
+			System.out.println(" Invalild Arguments: Provide argument.");
+			System.exit(1);
+		} else {
+			PORTNO = Integer.parseInt(args[0]);
+			SERVER_IP = args[1];
+			SERVER_PORT = Integer.parseInt(args[2]);
+			// check portno is of length 4 and in range.
+		}
+
+		new SimpleThread(PORTNO).start();
+		// Thread.sleep(10000);
+		TCPClient.createClient(SERVER_IP, SERVER_PORT);
 	}
- }
+}
 
 class TCPServer {
 	public static void createServer(int portNo) throws IOException {
@@ -22,31 +39,76 @@ class TCPServer {
 		ServerSocket welcomeSocket = new ServerSocket(portNo);
 
 		while (true) {
-			System.out.println("Server .. Listening : ");
-			Socket connectionSocket = welcomeSocket.accept();
+			System.out.println("******Server .. Listening : ");
+			Socket connectionSocket = welcomeSocket.accept(); // Blocking
+																// statement
+
 			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-			clientSentence = inFromClient.readLine();
-			System.out.println("Server ..... Received: " + clientSentence);
-			capitalizedSentence = clientSentence.toUpperCase() + '\n';
-			outToClient.writeBytes(capitalizedSentence);
+
+			do { // do while till terminate called.
+
+				System.out.println("Server: Waiting for client to request..");
+				clientSentence = inFromClient.readLine(); // Blocking statement
+
+				System.out.println("Server ..... Received: " + clientSentence);
+				if (clientSentence.equals("terminate")) {
+					System.out.println("Server: Bye bye client...");
+					outToClient.writeBytes(clientSentence);
+				} else {
+					capitalizedSentence = clientSentence.toUpperCase() + '\n';
+					outToClient.writeBytes(capitalizedSentence);
+
+				}
+			} while (!clientSentence.equals("terminate"));
+
+			connectionSocket.close();
 		}
 	}
 }
 
-class TCPClient{
-	public static void createClient(String serverIP, int portNO) throws UnknownHostException, IOException{
-		String sentence;
+class TCPClient {
+	public static void createClient(String serverIP, int portNO) throws UnknownHostException, IOException {
+		String sentence = "";
 		String modifiedSentence;
-		BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-		Socket clientSocket = new Socket("localhost", portNO);
+
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+		Socket clientSocket = new Socket(serverIP, portNO);
+
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		System.out.println("Read from console:");
-		sentence = inFromUser.readLine();
-		outToServer.writeBytes(sentence+"\n");
-		modifiedSentence = inFromServer.readLine();
-		System.out.println(" From Server: "+modifiedSentence);
+
+		do {
+			System.out.println("Client: Input from console:");
+			sentence = inFromUser.readLine();
+
+			outToServer.writeBytes(sentence + "\n");
+			modifiedSentence = inFromServer.readLine();
+			System.out.println("Client:  From Server: " + modifiedSentence);
+
+		} while (!sentence.equals("terminate"));
+
 		clientSocket.close();
-	 }
-  }
+		System.exit(0);
+	}
+}
+
+class SimpleThread extends Thread {
+	private int portNO;
+
+	public SimpleThread(Object portNO) {
+		super();
+		this.portNO = (int) portNO;
+	}
+
+	@Override
+	public void run() {
+		try {
+			TCPServer.createServer(portNO);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
